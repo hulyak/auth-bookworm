82944f36a83d9e22b0b3ae1b530b503d1a4cba03
hulya <hulyakarakayaa06@gmail.com>
2021-01-22 11:31:16 -0800

    Use session variables to customize content

--- app.js
+++ app.js
@@ -12,6 +12,12 @@ app.use(session( {
   saveUninitialized: false
 }));
 
+// make user ID available in templates
+app.use((req, res, next) => {
+  res.locals.currentUser = req.session.userId;
+  next();
+})
+
 // mongodb connection
 mongoose.connect('mongodb://localhost:27017/bookworm', {
   useNewUrlParser: true,
