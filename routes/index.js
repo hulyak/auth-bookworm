6493f8ffeaf44dfffb268a1b99545092dd2a6a90
hulya <hulyakarakayaa06@gmail.com>
2021-01-21 19:09:08 -0800

    prepare the register route wit pug and route handlers

---
+++ routes/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+
+// GET /register
+router.get('/register', function(req, res, next) {
+  return res.render('register', {title: 'Sign Up'});
+});
+
+// POST /register
+router.post('/register', function(req, res, next) {
+  return res.send('Registered user');
+});
+
+// GET /
+router.get('/', function(req, res, next) {
+  return res.render('index', { title: 'Home' });
+});
+
+// GET /about
+router.get('/about', function(req, res, next) {
+  return res.render('about', { title: 'About' });
+});
+
+// GET /contact
+router.get('/contact', function(req, res, next) {
+  return res.render('contact', { title: 'Contact' });
+});
+
+module.exports = router;
